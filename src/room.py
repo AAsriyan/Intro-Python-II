7cbd2962b2612ba42f6d812e5767043d8d6140e7
AAsriyan <aasriyan@protonmail.com>
2019-05-29 16:14:07 -0400

    Initialized project, created room and player classes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+# n_to, s_to, e_to, w_to
+
+
+class Room:
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+        # self.n_to = n_to
+        # self.s_to = s_to
+        # self.e_to = e_to
+        # self.w_to = w_to
+
+    def __str__(self):
+        return f"From Room class: name is {self.name}, description is {self.desc}"
+
+# , North: {self.n_to}, North: {self.n_to}, South: {self.s_to}, East: {self.e_to}, West: {self.w_to}"
